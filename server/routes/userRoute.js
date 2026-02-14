a524b2d799e32116cdb15ffdfca2a07b0f22f8ab
thejaskrishnan <krishnanthejas50@gmail.com>
2026-02-14 21:40:28 +0530

    login route added

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import rateLimit from "express-rate-limit";
 
 import { signup } from "../controllers/userController.js";
+import { login } from "../controllers/userController.js";
 
 const signupLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -11,4 +12,6 @@ const signupLimiter = rateLimit({
 const router = express.Router();
 
 router.route("/signup").post(signup);
+router.route("/login").post(login);
+
 export default router;
