30e72a569c9130f65be48a8c7be857e2691aae53
thejaskrishnan <krishnanthejas50@gmail.com>
2026-02-14 21:38:38 +0530

    login added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,66 @@
 import bcrypt from "bcryptjs";
 
 import User from "../models/userModel.js";
+import generateTokens from "../utils/generateTokens.js";
 
-// export const getData = function (req, res) {
-//   try {
-//     res.status(200).json({ message: "Successfull" });
-//   } catch (err) {
-//     console.log("error:", err.message);
-//     res.status(500).json({ message: "Failed", error: err.message });
-//   }
-// };
+export const login = async function (req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password required",
+        status: 400,
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Invalid credentials" });
+    }
+
+    const accessToken = generateTokens.generateAccessToken(user._id);
+    const refreshToken = generateTokens.generateRefreshToken(user._id);
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: "Login successfully",
+      user: {
+        id: user._id,
+        name: user.fullname,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.log("error:", err.message);
+    res
+      .status(500)
+      .json({ status: 500, message: "Failed", error: err.message });
+  }
+};
 
 export const signup = async function (req, res) {
   try {
