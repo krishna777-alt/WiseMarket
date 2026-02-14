277eeac646bb64c0a8b9a8e1ad058dbf7d0fe6cb
thejaskrishnan <krishnanthejas50@gmail.com>
2026-02-14 21:37:26 +0530

    Login page added

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,13 +1,50 @@
 import { Leaf, Mail, Lock } from "lucide-react";
 import { Navigate, NavLink, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+
+import API from "../api/axios";
+
+function SignupLink() {
+  return (
+    <p className="text-sm text-center text-gray-500">
+      Don’t have an account?
+      <NavLink
+        to="/signup"
+        className="ml-2 text-green-600 font-medium hover:underline"
+      >
+        Sign Up
+      </NavLink>
+    </p>
+  );
+}
+
+function LoginHeader({ errMessage }) {
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-center text-gray-900 mb-2">
+        Welcome Back 👋
+      </h2>
+      {errMessage && <p>{errMessage}</p>}
+      <p className="text-sm text-center text-gray-500 mb-8">
+        Login to continue shopping fresh groceries
+      </p>
+    </>
+  );
+}
 
 export default function Login() {
   const location = useLocation();
-
   const navigate = useNavigate();
+
+  const [formData, getFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (location.state?.message) {
@@ -15,9 +52,57 @@ export default function Login() {
       // window.history.replaceState({}, document.title);
       navigate(location.pathname, { replace: true });
     }
-    return () => {};
   }, [location, navigate]);
 
+  function handleChange(e) {
+    getFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  const validateForm = function () {
+    if (!formData.email || !formData.password) {
+      return "All fields are required";
+    }
+
+    if (formData.password.length < 6) {
+      return "Invalied credentials";
+    }
+
+    return null;
+  };
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const validationError = validateForm();
+    if (validationError) {
+      setError(validationError);
+      return;
+    }
+    try {
+      (setLoading(true), setError(""));
+
+      const response = await API.post("/client/login", formData);
+
+      console.log("response:", response.data.status);
+
+      if (response.data.status === 200) {
+        toast.success(response.data.message);
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(`ERROR:${err.message}`);
+      setError(
+        err.response?.data?.message || "Login failed,please try agin later",
+      );
+    } finally {
+      getFormData({
+        email: "",
+        password: "",
+      });
+    }
+  }
   return (
     <div
       className="min-h-screen flex items-center justify-center bg-cover bg-center"
@@ -32,99 +117,114 @@ export default function Login() {
                   rounded-3xl p-8 shadow-2xl text-white"
       >
         {/* Logo */}
-        <div className="flex justify-center mb-6">
-          <div
-            className="h-12 w-12 rounded-2xl 
-                          bg-gradient-to-br from-green-500 to-emerald-600 
-                          flex items-center justify-center shadow-md"
-          >
-            <Leaf className="h-6 w-6 text-white" />
-          </div>
-        </div>
+        <Logo />
 
         {/* Title */}
-        <h2 className="text-2xl font-bold text-center text-gray-900 mb-2">
-          Welcome Back 👋
-        </h2>
-
-        <p className="text-sm text-center text-gray-500 mb-8">
-          Login to continue shopping fresh groceries
-        </p>
+        <LoginHeader errMessage={error} />
 
         {/* Form */}
-        <form className="space-y-5">
-          {/* Email */}
-          <div className="relative">
-            <Mail
-              className="absolute left-4 top-1/2 -translate-y-1/2 
+        <Form
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          formData={formData}
+          loading={loading}
+        />
+
+        {/* Divider */}
+        <Divider />
+
+        {/* Sign Up Link */}
+        <SignupLink />
+      </div>
+    </div>
+  );
+}
+
+function Form({ onChange, onSubmit, formData, loading }) {
+  return (
+    <form onSubmit={onSubmit} className="space-y-5">
+      {/* Email */}
+      <div className="relative">
+        <Mail
+          className="absolute left-4 top-1/2 -translate-y-1/2 
                               h-4 w-4 text-gray-400"
-            />
-            <input
-              type="email"
-              placeholder="Email address"
-              className="w-full pl-11 pr-4 py-3 rounded-xl 
+        />
+        <input
+          type="email"
+          name="email"
+          onChange={onChange}
+          value={formData.email}
+          placeholder="Email address"
+          className="w-full pl-11 pr-4 py-3 rounded-xl 
                          border border-gray-200 
                          focus:ring-2 focus:ring-green-500/40 
                          focus:border-green-500 
                          outline-none transition"
-            />
-          </div>
+        />
+      </div>
 
-          {/* Password */}
-          <div className="relative">
-            <Lock
-              className="absolute left-4 top-1/2 -translate-y-1/2 
+      {/* Password */}
+      <div className="relative">
+        <Lock
+          className="absolute left-4 top-1/2 -translate-y-1/2 
                               h-4 w-4 text-gray-400"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full pl-11 pr-4 py-3 rounded-xl 
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={onChange}
+          value={formData.password}
+          placeholder="Password"
+          className="w-full pl-11 pr-4 py-3 rounded-xl 
                          border border-gray-200 
                          focus:ring-2 focus:ring-green-500/40 
                          focus:border-green-500 
                          outline-none transition"
-            />
-          </div>
+        />
+      </div>
 
-          {/* Forgot Password */}
-          <div className="flex justify-end">
-            <a href="#" className="text-xs text-green-600 hover:underline">
-              Forgot Password?
-            </a>
-          </div>
+      {/* Forgot Password */}
+      <div className="flex justify-end">
+        <NavLink href="#" className="text-xs text-green-600 hover:underline">
+          Forgot Password?
+        </NavLink>
+      </div>
 
-          {/* Login Button */}
-          <button
-            type="submit"
-            className="w-full py-3 rounded-xl 
+      {/* Login Button */}
+      <button
+        type="submit"
+        className="w-full py-3 rounded-xl 
                        bg-gradient-to-r from-green-500 to-emerald-600 
                        text-white font-semibold 
                        shadow-lg hover:shadow-xl 
                        hover:scale-[1.02] 
                        transition-all duration-300"
-          >
-            Login
-          </button>
-        </form>
+      >
+        {loading ? "Login in ..." : "Login"}
+      </button>
+    </form>
+  );
+}
 
-        {/* Divider */}
-        <div className="my-6 flex items-center gap-3">
-          <div className="flex-1 h-px bg-gray-200"></div>
-          <span className="text-xs text-gray-400">OR</span>
-          <div className="flex-1 h-px bg-gray-200"></div>
-        </div>
+function Divider() {
+  return (
+    <div className="my-6 flex items-center gap-3">
+      <div className="flex-1 h-px bg-gray-200"></div>
+      <span className="text-xs text-gray-400">OR</span>
+      <div className="flex-1 h-px bg-gray-200"></div>
+    </div>
+  );
+}
 
-        {/* Sign Up Link */}
-        <p className="text-sm text-center text-gray-500">
-          Don’t have an account?
-          <NavLink
-            to="/signup"
-            className="ml-2 text-green-600 font-medium hover:underline"
-          >
-            Sign Up
-          </NavLink>
-        </p>
+function Logo() {
+  return (
+    <div className="flex justify-center mb-6">
+      <div
+        className="h-12 w-12 rounded-2xl 
+                          bg-gradient-to-br from-green-500 to-emerald-600 
+                          flex items-center justify-center shadow-md"
+      >
+        <Leaf className="h-6 w-6 text-white" />
       </div>
     </div>
   );
