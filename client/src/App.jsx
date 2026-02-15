6268826e033ee5bc2a251f6058fbe060481cc5e4
thejaskrishnan <krishnanthejas50@gmail.com>
2026-02-15 12:59:44 +0530

    Added AuthContext to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,26 @@ import Layout from "./Components/Layout";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./Auth/AuthContext";
+import Cart from "./Pages/Cart";
+import Forbidden403 from "./Pages/Forbidden403";
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="*" element={<Forbidden403 />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
